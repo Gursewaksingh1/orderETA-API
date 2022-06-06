22b4970c5c17f3d4f00ba5b364bed87e75c1ccc4
Himanshu kumar <74171370+himanshu064@users.noreply.github.com>
2022-06-06 02:13:23 -0700

    Update drivers.js

--- router/drivers.js
+++ router/drivers.js
@@ -2,8 +2,10 @@ const express = require('express');
 const driverController = require('../controller/drivers');
 const router = express.Router();
 const isAuth = require("../middleware/isAuth")
+const driverLoginSchema = require('../validatorSchema/driver_login_validation')
+const {body, checkSchema} = require('express-validator');
 
-router.post('/login', driverController.login)
+router.post('/login',checkSchema(driverLoginSchema), driverController.login)
 router.post('/refreshtoken', driverController.refreshToken)
 
 module.exports = router;
