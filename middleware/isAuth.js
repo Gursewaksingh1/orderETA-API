d7e4b46497684bc1fc7f6582ebb818c91c530cd6
Himanshu kumar <74171370+himanshu064@users.noreply.github.com>
2022-06-05 23:56:04 -0700

    Update isAuth.js

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,12 +1,17 @@
 const jwt = require("jsonwebtoken");
 const Driver = require("../model/driverModel");
 
-isAuth = async (req, res,next) => {
- const auth = req.headers["authorization"];
+isAuth = async (req, res, next) => {
+  const auth = req.headers["authorization"];
   const token = auth && auth.split(" ")[1];
 
   if (!token) {
-    return res.status(403).send("A token is required for authentication");
+    return res
+      .status(403)
+      .send({
+        status: "failed",
+        error: "A token is required for authentication",
+      });
   }
   try {
     data = jwt.verify(token, process.env.SECRET);
@@ -15,7 +20,7 @@ isAuth = async (req, res,next) => {
     if (user == null) {
       return res
         .status(403)
-        .send({ status: "failed", msg: "the owner of this token does not exist" });
+        .send({ status: "failed", error: "the owner of this token does not exist" });
     } else {
       req.user = data;
       next();
