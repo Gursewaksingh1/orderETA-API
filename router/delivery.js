4c547da5d070761b238431bb0ad91ef2071648ba
Himanshu kumar <74171370+himanshu064@users.noreply.github.com>
2022-06-08 15:25:54 +0530

    Add files via upload

---
+++ router/delivery.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const deliveryController = require("../controller/delivery");
+const router = express.Router();
+const isAuth = require("../middleware/isAuth");
+const {
+  deliveryValidationRules,
+  updateDeliveryValidationRules,
+} = require("../validatorSchema/deliveryValidationRules");
+const validate = require("../validatorSchema/validateMiddleware");
+
+router.post(
+  "/",
+  isAuth,
+  deliveryValidationRules(),
+  validate,
+  deliveryController.postDelivery
+);
+router.put(
+  "/",
+   isAuth,
+  updateDeliveryValidationRules(),
+  validate,
+ 
+  deliveryController.updateDelivery
+);
+router.delete('/',isAuth,deliveryController.deleteDelivery)
+module.exports = router;
