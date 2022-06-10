5f09d842354b93578cbe2dab95fd2ec73e43cb14
Himanshu kumar <74171370+himanshu064@users.noreply.github.com>
2022-06-10 15:08:39 +0530

    Add files via upload

---
+++ middleware/isAuth.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const User = require("../model/_User");
+
+isAuth = async (req, res, next) => {
+  const auth = req.headers["authorization"];
+  const token = auth && auth.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(403)
+      .send({
+        status: "failed",
+        error: "A token is required for authentication",
+      });
+  }
+  try {
+    data = jwt.verify(token, process.env.SECRET);
+
+    user = await User.findOne({username:data.userName,passowrd:data.userPassword});
+    if (user == null) {
+      return res
+        .status(403)
+        .send({ status: "failed", error: "the owner of this token does not exist" });
+    } else {
+      req.user = data;
+      next();
+    }
+  } catch (err) {
+    return res.status(403).send({ status: "failed", error: err });
+  }
+};
+
+module.exports = isAuth;
