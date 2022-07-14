a262877a0d67fb9440174431993b3357f1794034
Himanshu <himanshu064@gmail.com>
2022-07-14 16:32:49 +0530

    updated seq endpint

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ const options = {
       version: "1.0.0",
     },
     servers:[{
-     url:"http://localhost:3000/api",
-     //url:"https://order-eta-app.herokuapp.com/api"
+     //url:"http://localhost:3000/api",
+     url:"https://order-eta-app.herokuapp.com/api"
     }
      
     ],
